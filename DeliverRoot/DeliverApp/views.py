27fecd297d59eca7e627d57364710e0787b2890e
isaacfc4 <DBdb234200!>
2020-04-16 11:16:03 -0400

    Finally Fixed Structure
    Created first website

--- DeliverRoot/DeliverApp/views.py
+++ DeliverRoot/DeliverApp/views.py
@@ -1,3 +1,10 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+    current_user = "Isaac Chapman"
+    return render(request, 'home.html',{'date': datetime.now(),'login' : current_user})
